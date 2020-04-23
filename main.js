754ff7cba71cbabdf87fcfbdd92218b3b92bb750
Kisha Robinson <35880413+kishanadine@users.noreply.github.com>
2020-04-22 23:19:23 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,12 @@
+
+
+/*------------------------------- Mobile Menu -----------------------------*/
+/* Open */
+function openNav() {
+    document.getElementById("myNav").style.height = "100%";
+  }
+  
+  /* Close */
+  function closeNav() {
+    document.getElementById("myNav").style.height = "0%";
+  }
